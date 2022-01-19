fc89aea17326e1c55f7157cecfac8712842e466a
anyushu <suzuki@lamp.jp>
2022-01-19 18:06:44 +0900

    upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,4 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   mode: 'jit',
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    colors: {
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-
-      red: colors.red,
-      yellow: colors.yellow,
-      blue: colors.blue,
-      green: colors.green,
-    },
-    extend: {},
-  },
-  variants: {
-    extend: {
-      textColor: ['active'],
-    },
-  },
-  plugins: [],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
 }
